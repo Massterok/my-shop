017786f9c71d203b13d1795acfcad5e25153da1c
Massterok <masterok.zprr@gmail.com>
2025-03-05 15:51:03 +0200

    Исправлен fetch и CORS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function loadData() {
-    const url = 'https://script.google.com/macros/s/AKfycbxpiItO7qxoGkQYrEMimXJrYgt-UhwHDnGWr4DdYG0tXtV0XnIZQIxSrGJxajB3eEAP/exec';
-
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=U8bF69zqhPKVPnHwgIWIHcakZf1gqN-JZdmF-1EFe8SC8Z2bxSeFSOk17yDnpspfksvCVaLgX19klfO6ypFUj7NXCnYvoQCVm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLQv_eivzQ8foSMLX0kPVHppgkBDadbybQyDMlFBzmoxPvVhVU80XmO0N-nCLmnucrGGDlWngRj8Sl7Ha0FhY5evrj2w6-IW8Q&lib=Mi861rc2CdYLI1pP_kmqJYrrONyUf71mb';
+    
     try {
         let response = await fetch(url);
         
